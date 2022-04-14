7d635c5fc1a0afe829748ce72e391e78393a4141
dapontesmith <65298051+dapontesmith@users.noreply.github.com>
2022-04-14 12:07:59 -0400

    added context and redist change variables

--- find_movers_to_wave21.R
+++ find_movers_to_wave21.R
@@ -12,9 +12,9 @@ library(haven)
 numCores = detectCores()
 registerDoParallel(numCores - 1)
 
-#setwd("C:/Users/dapon/Dropbox/Harvard/dissertation")
-#df <- read_dta("data/bes/internet_panel/BES2019_W21_Panel_v21.0.dta") 
-df <- read_dta("BES2019_W21_Panel_v21.0.dta")
+setwd("C:/Users/dapon/Dropbox/Harvard/dissertation")
+df <- read_dta("data/bes/internet_panel/BES2019_W21_Panel_v21.0.dta") 
+#df <- read_dta("BES2019_W21_Panel_v21.0.dta")
 
 #dplyr::select only the geographic variables
 df <- df %>%
@@ -98,7 +98,102 @@ pcon_out <- unpack_result(result_pcon) %>%
   # filter out people who weren't in a constituency, or aren't in one now
   filter(new_area != "NOT in a 2010 Parliamentary Constituency" & 
            old_area != "NOT in a 2010 Parliamentary Constituency")
+
+# now match the consituencies to income data
+
+# read in the pcon data 
+pcon_income <- read_csv("data/uk_geography/pcon_data/pcon_income_data_2018.csv") 
+pcon_income <- pcon_income %>% 
+  select(PCON19CD, PCON19NM,
+         median_total_income) %>% 
+  # make the variable numeric
+  mutate(median_total_income = as.numeric(str_remove(median_total_income, ",")),# , 
+         # make scaled version of the variable 
+         median_total_income_scale = scale(median_total_income))
+
+# join with pcon out - do this twice, to account for new and old 
+pcon_join <- left_join(pcon_out, pcon_income, 
+                  by = c("new_area" = "PCON19NM")) %>% 
+  left_join(., pcon_income, 
+            by = c("old_area" = "PCON19NM"),
+            suffix = c("_new","_old")) %>% 
+  # make variables for relative and abolsute change between new and old places
+  mutate(new_old_diff_absolute = median_total_income_new - median_total_income_old, 
+         new_old_diff_relative = median_total_income_scale_new - median_total_income_scale_old)
+
+# get only the most recent move 
+pcon_join_recent <- pcon_join %>% 
+  group_by(id) %>% 
+  mutate(n = 1:n()) %>% 
+  filter(n == max(n))
+
+# read in redistribution data
+redist <- read_csv("data/bes/internet_panel/bes_redist_wave1_to_21.csv")
+
+# make long version of redistribution data 
+redist_long <- redist %>% 
+  pivot_longer(cols = redistSelfW1:redistSelfW21,
+               names_to = "wave",
+               values_to = "redistSelf") %>% 
+  mutate(wave = as.numeric(str_remove(wave, "redistSelfW"))) %>% 
+  mutate(redistSelf = ifelse(redistSelf == 9999, NA, redistSelf)) %>% 
+  filter(!is.na(redistSelf)) %>%
+  # switch around scaleof redist such that higher = more supportive 
+  mutate(redistSelf = max(redistSelf, na.rm = TRUE) - redistSelf)
+
+# get wave 21 data 
+redist_wave21 <- redist_long %>% 
+  filter(wave == 21)
+
+# get df of ids and wave moved from pcon_join_recent 
+redist_full <- pcon_join_recent %>% 
+  select(id, wave_moved) %>% 
+  mutate(wave_moved = as.numeric(wave_moved)) %>% 
+  left_join(., redist_long, 
+            by = c("id","wave_moved" = "wave")) %>% 
+  left_join(., redist_wave21 %>% 
+              select(-wave), 
+            by = "id",
+            suffix = c("_orig","_wave21")) %>% 
+  mutate(redist_diff = redistSelf_wave21 - redistSelf_orig) 
   
+
+
+
+# read in bes data
+bes <- read_csv("data/bes/internet_panel/clean_data/bes_wave21_clean.csv")
+
+# join bes to pcon_join_recent, by id 
+test <- left_join(bes, 
+                  pcon_join_recent %>% 
+                    select(id, starts_with("median"),
+                           starts_with("new_old")), 
+                  by = "id") %>% 
+  # join in the redist_full df
+  left_join(., redist_full,
+            by = "id")
+
+# Do movers become more supportive of redistribution when they move to poorer places? 
+local_redist <- lm_robust(data = test, 
+                          redist_diff ~ new_old_diff_relative + p_edlevel + age + male + p_socgrade + 
+                            p_gross_household + leftRight,
+                          fixed_effects = region, clusters = PCON19CD)
+modelsummary(local_redist, 
+             stars = TRUE,
+            # output = "markdown",
+             coef_map = c("new_old_diff_relative" = "Relative difference between new and old constituency income", 
+                          "p_edlevel" = "Education",
+                          "age" = "Age",
+                          "male" = "Male",
+                          "p_socgrade" = "Social grade",
+                          "p_gross_household" = "Household income",
+                          "leftRight" = "Left-right self-placement"),
+             coef_omit = "factor",
+             title = "Change in context and change in preferences over redistribution among British movers",
+             notes = "Linear models with region fixed effects.
+            Dependent variable is change in preferences over redistribution over period of moving.")
+
+
 
 # do the same for the region data 
 gor_data <- in_wave_21 %>% 
@@ -129,12 +224,16 @@ out <- left_join(gor_out, pcon_out,
           by = "id",
           suffix = c("_gor",'_pcon'))
 
+
+
+
+
 # what to do about people who moved multiple times ? 
 
 
 # read in region income data
-# region_income <- read_csv("data/region_data/region_gva_indices_historical.csv")
-region_income <- read_csv("region_gva_indices_historical.csv") %>% 
+ region_income <- read_csv("data/region_data/region_gva_indices_historical.csv") %>% 
+#region_income <- read_csv("region_gva_indices_historical.csv") %>% 
   slice(-(1:3))
 
 names(region_income) <- c("period",
@@ -153,5 +252,5 @@ region_income <- region_income %>%
 
 #  get the 2021 data to match to regions 
 
-# now match the consituencies to income data
+
 
